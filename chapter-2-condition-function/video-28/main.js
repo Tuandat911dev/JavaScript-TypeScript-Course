b3b5e5ef5d0e9f2a9ea421049e7eb5f717e2a92f
PhamTuanDat <pdat100111@gmail.com>
2025-09-24 19:40:00 +0700

    video 28: loop

---
+++ chapter-2-condition-function/video-28/main.js
@@ -0,0 +1,29 @@
+console.log("Video 28");
+
+//  Loop
+
+// for(init, condition, update)
+const fruits = ["Apple", "Orange", "Banana"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// while
+let score = 5;
+
+while (score >= 5) {
+    console.log("Qua môn");
+    score -= score;
+}
+
+// do while
+let number = 10;
+do {
+    console.log("Enter 10, please!");
+    number--;
+} while(number !== 9);

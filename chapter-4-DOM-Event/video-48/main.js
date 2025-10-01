ebf4c5b61d94c685c3d7c0bbfb637c4d00fc7f40
PhamTuanDat <pdat100111@gmail.com>
2025-10-01 16:00:06 +0700

    video  48: Giới thiệu DOM

---
+++ chapter-4-DOM-Event/video-48/main.js
@@ -0,0 +1,14 @@
+console.log("Video 48: Giới thiệu DOM");
+
+/**
+- DOM - Document Object Model
+- DOM chính là một cây HTML, một trang web các phần tử tồn tại theo dạng cây kế thừa
+- JS thông qua DOM, có thể tác động lên các phần tử HTML
+Document
+ └── html
+     ├── head
+     │   └── title
+     └── body
+         ├── h1
+         └── p
+ */
